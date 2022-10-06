c0ec30428f9b443d8ba5e25e30bb2e7b8ae60b52
caleb <calebakpan7@gmail.com>
2022-10-07 00:25:09 +0100

    set up the JSON server in apiSlice.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import {useGetBooksQuery} from './features/api/apiSlice'
 
 function App() {
+  const {data, isSucess, isLoading, isError, error} = useGetBooksQuery()
+
+  isSucess && console.log(data)
   return (
     <div className="App">
       Learn React
---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,23 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000'}),
+    tagTypes: ['book'],
+    endpoints: builder => ({
+        getBooks: builder.query({
+            query: () => '/books',
+            transformResponse: res => (
+                console.log(res)
+            )
+        }),
+        getAuthor: builder.query({
+            query: () => '/author',
+        })
+    })
+})
+
+export const {
+    useGetBooksQuery,
+    useGetAuthorQuery
+} = apiSlice
